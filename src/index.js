b0640113ba50102e6bd177192509b42629440831
HenokG <heni.gebrehiwot@gmail.com>
2023-08-04 19:44:28 -0600

    Show Random Destination

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import Header from './layout/Header';
 import DestinationIndex from './components/DestinationIndex';
 import { Provider } from "react-redux";
 import { store } from "./Redux/store";
+import RandomDestination from "./components/RandomDestination";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <Header />
       <DestinationIndex />
+      <RandomDestination />
     </Provider>
   </React.StrictMode>
 );
